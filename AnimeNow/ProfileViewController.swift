cd526f0f2e14992deb9d82260f6e833824762f66
Paul Chavarria <pjchavarria@gmail.com>
2016-04-16 01:19:34 -0700

    set 5 minimum likes to be popular

--- AnimeNow/ProfileViewController.swift
+++ AnimeNow/ProfileViewController.swift
@@ -378,7 +378,7 @@ class ProfileViewController: ThreadViewController {
             followingQuery.limit = 1000
             queryBatch.whereQuery(query, matchesKey: "postedBy", onQuery: followingQuery)
         case .Popular:
-            query.whereKey("likeCount", greaterThan: 9)
+            query.whereKey("likeCount", greaterThan: 4)
         case .Me:
             query.whereKey("userTimeline", equalTo: userProfile!)
         }
